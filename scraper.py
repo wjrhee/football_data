9cbe0033b1f43dbd80f95bf05716ed40608e61df
rheemansum <rheewalt@gmail.com>
2016-12-11 16:30:30 -0600

    notes

--- scraper.py
+++ scraper.py
@@ -1,3 +1,5 @@
+# TODO - scrape from player pages
+
 import bs4
 import requests
 
@@ -16,7 +18,7 @@ def getHeaders(page):
         'headersDescription': headersDescription
     }
 
-# pass in 'td' for pro-football-reference
+# pass in 'tr' for pro-football-reference
 def scraping(page, tag):
     allData = []
     playerData = []
@@ -31,14 +33,29 @@ def scraping(page, tag):
         playerData = []
     return allData
 
+def scrape(page, tag):
+    allData = []
+    playerData = []
+    queue = []
+    htmlPage = requests.get(page)
+    soup = bs4.BeautifulSoup(htmlPage.content, 'html.parser')
+    tagSoup = soup.find_all(tag)
+    for player in tagSoup:
+        linkTag = player.a
+        if(linkTag is not(None)):
+            # link = "http://www.pro-football-reference.com" + str(linkTag['href'])
+            # htmlPage = requests.get(link)
+            # newSoup = bs4.BeautifulSoup(htmlPage.content, 'html.parser')
+            # print(newSoup)
+            for stat in player.contents:
+                if (hasattr(stat, 'data-stat')):
+                    playerData.append(stat.string)
+            allData.append(playerData)
+        playerData = []
+    return allData
 
+scrape("http://www.pro-football-reference.com/years/2016/passing.htm", 'tr')
     # # TEST ONE DATA SET
     # i = tagSoup[1]
     # for j in i.contents:
     #     print(j)
-
-
-# testPage = "http://www.pro-football-reference.com/years/2016/passing.htm"
-# testTag = 'tr'
-# x = scraping(testPage, testTag)
-# print(x)
